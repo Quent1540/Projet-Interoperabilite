8a657bfa37bf91b6ba052cb788a138f88deb2574
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-12-10 16:44:15 +0100

    affichage qui marche avec une IP donnée

--- Atmosphere/public/php/index.php
+++ Atmosphere/public/php/index.php
@@ -1,12 +1,20 @@
 <?php
+//Récupération de l'ip du client
+$clientIP = $_SERVER['REMOTE_ADDR'];
 
+//Chargement le xml de la geolocalisation du client
 $XML = new DOMDocument();
-$XML->load( '');
+$XML->load( "http://ip-api.com/xml/24.48.0.1");
 
+//Les données qu'on veut qu'on recup depuis le xml
+$lat = $XML->getElementsByTagName("lat")->item(0)->nodeValue;
+$lon = $XML->getElementsByTagName("lon")->item(0)->nodeValue;
+$city = $XML->getElementsByTagName("city")->item(0)->nodeValue;
+
+//Transformation xslt
 $xslt = new XSLTProcessor();
-
 $XSL = new DOMDocument();
-$XSL->load( 'meteo.xsl' );
+$XSL->load( '../xsl/meteo.xsl' );
 $xslt->importStylesheet( $XSL );
 print $xslt->transformToXML( $XML );
 ?>
